705cb4d72e5c26f45155924294b8f51633008234
kevin <kevin@kevin-P480>
2015-02-12 20:54:20 -0600

    Creacion de la ventana Principal para usuarios

--- src/edd/practica_1/EDDPractica_1.java
+++ src/edd/practica_1/EDDPractica_1.java
@@ -16,6 +16,8 @@ public class EDDPractica_1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //crea una nueva ventada
+        new Principal().setVisible(true);
     }
     
 }
